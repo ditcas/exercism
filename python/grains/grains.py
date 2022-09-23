2194b07c6d5b1d9148a1baec72121b11e51376c0
ditcas <juditcastro.v@gmail.com>
2022-09-23 18:01:04 +0200

    feat: Afegeix exercici fet grains.py

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,19 @@
 def square(number):
-    pass
+    try:
+        number = int(number)
+    except TypeError as err:
+        raise print(f"Error: {err}")
+
+    if (number <= 0) or (number > 64):
+        raise ValueError("square must be between 1 and 64")
+    
+    return 2**(number -1)
 
 
 def total():
-    pass
+    total_grains = 0
+
+    for number in range(1, 65):
+        total_grains += square(number)
+
+    return total_grains
